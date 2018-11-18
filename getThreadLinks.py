e52105bbd493e7abea50e46a1427545fb6401967
ayxemma <wintersunrise11@gmail.com>
2018-11-17 22:32:36 -0500

    add code to script top 50 coins

--- getThreadLinks.py
+++ getThreadLinks.py
@@ -1,29 +1,53 @@
 from selenium import webdriver
 import time
 
-num_scrolls=10
+def get_url():
+    """
+    get top 50 market cap coins url in reddit
+    """
+    urllst=[]
+    prefix='https://www.reddit.com/r/'
+    with open('coinlist.txt') as f:coinlst=f.readlines()
+    coin_exclude=['Zcash', 'BitcoinGold','0x', 'Populous', 'Status']
+    coinlst = [x for x in coinlst if x not in coin_exclude]
+    for coin in coinlst:
+        urllst.append(prefix + coin.split(':')[0].replace(" ", "") + '/')
 
-url='https://www.reddit.com/r/btc/'
+    return urllst
+
 
 #open the browser and visit the url
 #driver = webdriver.Chrome('chromedriver.exe')
-driver=webdriver.Chrome(executable_path=r"/usr/local/bin/chromedriver")
-driver.get(url)
+def url_test():
+    urllst=get_url()
+    driver=webdriver.Chrome(executable_path=r"/usr/local/bin/chromedriver")
+    for url in urllst:
+        driver.get(url)
 
-done=set()
-fw=open('threadLinks.txt','w')
-for i in range(num_scrolls):
-
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(2)
-
-    threads=driver.find_elements_by_css_selector('[data-click-id="comments"]')
-
-    for thread in threads:
-        link=thread.get_attribute('href')
-        if link in done:continue
-        done.add(link)
-        fw.write(link+'\n')
-    
-
-fw.close()
+def get_threadlinks(urllst):
+    num_scrolls=10
+    for url in urllst:
+        coin=url.split('/')[-2]
+        driver=webdriver.Chrome(executable_path=r"/usr/local/bin/chromedriver")
+        driver.get(url)
+        
+        done=set()
+        fw=open(coin + '_threadLinks.txt','w')
+        for i in range(num_scrolls):
+        
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(2)
+        
+            threads=driver.find_elements_by_css_selector('[data-click-id="comments"]')
+        
+            for thread in threads:
+                link=thread.get_attribute('href')
+                if link in done:continue
+                done.add(link)
+                fw.write(link+'\n')
+            
+        
+        fw.close()
+if __name__ == "__main__":
+    urllst=get_url()
+    get_threadlinks(urllst)
